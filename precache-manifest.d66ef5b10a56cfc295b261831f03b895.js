e905025e18111bf2a405affb7bc5b10f0882bd71
Tatiana-256 <tatiana.beznosiuk@gmail.com>
2020-06-07 17:42:13 +0300

    Updates

---
+++ precache-manifest.d66ef5b10a56cfc295b261831f03b895.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68525c05f4ab09fed9d9f592ca2db65a",
+    "url": "/Clearmove/index.html"
+  },
+  {
+    "revision": "b5722f2c7a1616013e97",
+    "url": "/Clearmove/static/css/main.29fd4d2e.chunk.css"
+  },
+  {
+    "revision": "cbb0175f8353bdd80418",
+    "url": "/Clearmove/static/js/2.d578774f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Clearmove/static/js/2.d578774f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5722f2c7a1616013e97",
+    "url": "/Clearmove/static/js/main.0ba6ed4f.chunk.js"
+  },
+  {
+    "revision": "378dabcc0cc3ca144d46",
+    "url": "/Clearmove/static/js/runtime-main.a62a904e.js"
+  }
+]);
